59df0f3dd11c4e8202c1d554d7d754d061026454
Verner Rafael <vernerrrferreira@gmail.com>
2025-02-18 22:50:30 -0300

    First Commit

---
+++ android/app/src/main/kotlin/com/example/papacapim/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.papacapim
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/HomePage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => HomePageState();
+
+}
+
+class HomePageState extends State {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text('PapaCappim', style: TextStyle(color: Colors.white),textAlign: TextAlign.center ),
+        backgroundColor: Colors.black,
+      ),
+    );
+  }
+}
