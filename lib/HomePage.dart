7703c6630a6bf4ff94be32ea41385d392a343d7f
Verner Rafael <vernerrrferreira@gmail.com>
2025-02-19 19:43:33 -0300

    Criação NavigationBar

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -7,13 +7,33 @@ class HomePage extends StatefulWidget{
 }
 
 class HomePageState extends State {
+  
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(
-        title: Text('PapaCappim', style: TextStyle(color: Colors.white),textAlign: TextAlign.center ),
+        title: Text('PapaCapim', style: TextStyle(color: Colors.white)),centerTitle: true,
         backgroundColor: Colors.black,
       ),
+      bottomNavigationBar: NavigationBar(destinations:  const <Widget>[
+          NavigationDestination(
+            icon: Icon(Icons.messenger_sharp),
+            label: 'Menssagens',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.account_circle_outlined),
+            label: 'Perfil',
+          ),
+        ],),
+      body: <Widget>[
+        Card(
+
+        )
+
+      ],
+      
+      
+      
     );
   }
 }
